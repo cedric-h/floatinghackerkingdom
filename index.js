a86e469ecb234ff6f472ec50f820270764c43c15
Cedric Hutchings <ced@misguided.enterprises>
2022-04-30 17:34:03 -0400

    ugh

--- index.js
+++ index.js
@@ -8,27 +8,11 @@ const ctx = canvas.getContext("2d");
   canvas.height = window.innerHeight;
 })();
 
-// const { channels: names, data: proximities } =
-//   await fetch("matrix.json").then(x => x.json());
-let names = [...Array(500)].map(x =>
-  [...Array(3)]
-    .map(() => Math.floor(Math.random() * 256))
-);
-const maxd = Math.sqrt(255*255 + 255*255 + 255*255);
-const proximities = names.map(r => names.map(c => {
-  const [rd, gd, bd] = r.map((rx, i) => rx - c[i]);
-  return Math.sqrt(rd*rd + gd*gd + bd*bd) / maxd;
-}));
+const { channels: names, data: proximities } =
+  await fetch("matrix.json").then(x => x.json());
+
 const points = v2.circle(proximities.length)
-  .map((x, i) => ({ i, pos: v2.mulf(x, Math.random() * 500) }));
-
-const mouse = { x: 0, y: 0 };
-window.onmousemove = ev => (mouse.x = ev.pageX -  canvas.width/2,
-                            mouse.y = ev.pageY - canvas.height/2);
-
-let mousedown = false;
-window.onmousedown = ev => mousedown = true;
-window.onmouseup = ev => mousedown = false;
+  .map((x, i) => ({ i, pos: v2.mulf(x, Math.random()*500) }));
 
 let search = "ced";
 window.onkeydown = e => {
@@ -39,31 +23,43 @@ window.onkeydown = e => {
 // const onlycare = points
 //   .map(p => [...points]
 //     .sort((a, b) => proximities[p.i][a.i] - proximities[p.i][b.i])
-//     .slice(0, 10))
+//     .slice(0, 10));
+
+let scale = 1;
+document.querySelector("canvas").onwheel = ev => {
+  scale = Math.max(0, scale + event.deltaY * -0.01);
+  ev.preventDefault();
+}
+
+let mousedown = false;
+window.onmousedown = ev => mousedown = true;
+window.onmouseup = ev => mousedown = false;
+
+let cam = vec();
+const mouse = vec();
+window.onmousemove = ev => {
+  if (!mousedown) {
+    mouse.x = (ev.pageX -  canvas.width/2) / scale;
+    mouse.y = (ev.pageY - canvas.height/2) / scale;
+  } else {
+    cam.x += ev.movementX;
+    cam.y += ev.movementY;
+  }
+}
 
 requestAnimationFrame(function frame(now) {
   ctx.fillStyle = "skyblue";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.save();
-  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.translate(cam.x + canvas.width/2, cam.y + canvas.height/2);
+  ctx.scale(scale, scale)
 
-  for (let i = 0; i < 5; i++) {
-  ctx.fillStyle = "darkblue";
+  let dpas = new Array(points.length);
   for (const point of points) {
     const { i, pos } = point;
 
-    const SCALE = 750;
-
-    const hover = Math.abs(pos.x - mouse.x) < 5 &&
-                  Math.abs(pos.y - mouse.y) < 5;
-
-    // if (hover && mousedown)
-    //   console.log(names[i]);
-    // ctx.fillStyle = hover ? "crimson" : "darkblue";
-    // if (names[i].startsWith(search)) ctx.fillStyle = "magenta";
-    const [r, g, b] = names[i];
-    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+    const SCALE = 1000;
 
     const { max, min, abs, sign } = Math;
 
@@ -71,19 +67,40 @@ requestAnimationFrame(function frame(now) {
     const goalprox = other => proximities[i][other.i];
     const deltaprox = other => realprox(other) - goalprox(other)
 
-    for (const other of points) if (other != point) {
-      let d = v2.norm(v2.sub(other.pos, pos));
-      point.pos = v2.add(pos, v2.mulf(d, deltaprox(other) * 5));
+    let dpa = 0;
+    for (let i = 0; i < 5; i++) {
+      for (const other of points) if (other != point) {
+        dpa += abs(deltaprox(other));
+
+        let d = v2.norm(v2.sub(other.pos, pos));
+        point.pos = v2.add(pos, v2.mulf(d, 5 + deltaprox(other) * 250));
+      }
     }
+    dpa /= (points.length - 1);
+    dpa /= 5;
+    dpas[i] = dpa;
+  }
+
+  const dpaMin = Math.min(...dpas);
+  const dpaMax = Math.max(...dpas);
+  for (const point of points) {
+    const { i, pos } = point;
+
+    let size = 10/scale;
+    const hover = Math.abs(cam.x / scale + pos.x - mouse.x) < size/2 &&
+                  Math.abs(cam.y / scale + pos.y - mouse.y) < size/2;
+
+    const scaledDpa = (dpas[i] - dpaMin) / dpaMax;
+    ctx.fillStyle = `rgb(0, ${scaledDpa * 255}, 0)`;
+    if (hover && mousedown)
+      console.log(names[i]);
+    if (hover)
+      ctx.fillStyle = "crimson";
+    if (names[i].startsWith(search))
+      ctx.fillStyle = "magenta";
 
     const { x, y } = pos;
-    ctx.globalAlpha = 1;
-    ctx.fillRect(x-5, y-5, 10, 10);
-    ctx.globalAlpha = 0.1;
-    ctx.fillRect(x-15, y-15, 30, 30);
-    ctx.globalAlpha = 0.03;
-    ctx.fillRect(x-50, y-50, 100, 100);
-  }
+    ctx.fillRect(x-size/2, y-size/2, size, size);
   }
 
   ctx.restore();
